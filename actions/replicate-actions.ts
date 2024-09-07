a34b78d07d1ef64867dcef9ceb390c688be25af6
Joseph Damiba <josephdamiba@Josephs-MacBook-Pro.local>
2024-09-07 15:28:09 -0700

    update input to image model

--- actions/replicate-actions.ts
+++ actions/replicate-actions.ts
@@ -19,17 +19,11 @@ export async function generateFluxImage(prompt: string) {
 
 export async function generateFluxProImage(prompt: string) {
   const input = {
-    prompt: prompt,
-    num_outputs: 1,
-    aspect_ratio: "1:1",
-    output_format: "webp",
-    output_quality: 80,
+    prompt:
+      "The world's largest black forest cake, the size of a building, surrounded by trees of the black forest",
   };
 
   const output = await replicate.run("black-forest-labs/flux-pro", { input });
-  console.log("output" + output);
-
-  return output;
 }
 
 export async function generateLightningImage(prompt: string) {
